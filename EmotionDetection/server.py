c07b2d7e8b92f44cfbf87548b092aef902208b6f
ChiefBeef <aaroneldinary@gmail.com>
2026-04-24 14:27:51 -0400

    Project424Step6

---
+++ EmotionDetection/server.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+from emotion_detection import emotion_detector
+app = Flask("Emotion Detection")
+app.debug = True
+
+@app.route("/emotionDetector", methods=['GET', 'POST'])
+def detect_route():
+    if request.method == 'POST':
+        data = request.get_json(silent=True)
+        text_to_analyze = None
+        if data:
+            text_to_analyze = data.get('textToAnalyze') 
+        if not text_to_analyze:
+            text_to_analyze = request.form.get('textToAnalyze')
+    else:
+        text_to_analyze = request.args.get('textToAnalyze')
+    if not text_to_analyze or not text_to_analyze.strip():
+        return "Error: please provide an input"
+    
+    response = emotion_detector(text_to_analyze)
+    anger = response.get('anger')
+    disgust = response.get('disgust')
+    fear = response.get('fear')
+    joy = response.get('joy')
+    sadness = response.get('sadness')
+    dominant_emotion = response.get('dominant emotion')
+    return (f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is <b>{dominant_emotion}</b>.")
+
+@app.route("/")
+def render_index_page():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
