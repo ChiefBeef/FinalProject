287bc2ed001abec427fdd9ca1df1ea1b7614479a
ChiefBeef <aaroneldinary@gmail.com>
2026-04-24 16:43:26 -0400

    424Step8

--- EmotionDetection/server.py
+++ EmotionDetection/server.py
@@ -1,20 +1,20 @@
+''' Server application '''
 from flask import Flask, render_template, request
-from emotion_detection import emotion_detector
+from EmotionDetection.emotion_detection import emotion_detector
 app = Flask("Emotion Detection")
-app.debug = True
-
+''' App route '''
 @app.route("/emotionDetector", methods=['GET', 'POST'])
 def detect_route():
+    ''' Emotion detection app'''
     if request.method == 'POST':
         data = request.get_json(silent=True)
         text_to_analyze = None
         if data:
-            text_to_analyze = data.get('textToAnalyze') 
+            text_to_analyze = data.get('textToAnalyze')
         if not text_to_analyze:
             text_to_analyze = request.form.get('textToAnalyze')
     else:
         text_to_analyze = request.args.get('textToAnalyze')
-    
     response = emotion_detector(text_to_analyze)
     anger = response.get('anger')
     disgust = response.get('disgust')
@@ -22,12 +22,16 @@ def detect_route():
     joy = response.get('joy')
     sadness = response.get('sadness')
     dominant_emotion = response.get('dominant emotion')
-    if dominant_emotion != None:
-        return (f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is <b>{dominant_emotion}</b>.")
-    else:
+    if dominant_emotion is None:
         return "<b>Invalid text! Please try again!</b>"
+    return (f"For the given statement, the system response is 'anger': {anger}, "
+            f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. "
+            f"The dominant emotion is <b>{dominant_emotion}</b>."
+    )
+
 @app.route("/")
 def render_index_page():
+    ''' render index page '''
     return render_template("index.html")
 
 if __name__ == "__main__":
